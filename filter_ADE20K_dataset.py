12a118297f0a2f26ad09a217a41e028062ada049
bogdan-stanciulescu <53901758+bogdan-s@users.noreply.github.com>
2019-10-24 23:16:09 +0300

    Update filter_ADE20K_dataset.py

--- filter_ADE20K_dataset.py
+++ filter_ADE20K_dataset.py
@@ -1,7 +1,10 @@
 import os 
 from shutil import copy2
+import cv2
+import multiprocessing
 
-ade20k_path = 'D:/Python/Datasets/ADE20K_2016_07_26/images/training/a/art_gallery' # where the ADE20K dataset is
+
+ade20k_path = 'D:/Python/Datasets/ADE20K_2016_07_26/images/training' # where the ADE20K dataset is
 # ade20k_path = 'D:/ADE20K_2016_07_26/images/training' # where the ADE20K dataset is
 
 dest = 'D:/Python/DataSets/ADE20K_Filtered' # where the filtered files are copied
@@ -9,6 +12,28 @@ dest = 'D:/Python/DataSets/ADE20K_Filtered' # where the filtered files are copie
 labels_to_filter = [" wall ", " ceiling ", " floor "]   # what label do you want to find
 
 file_count = 0
+
+processes = []
+
+def write_binary_mask(root, f, dest):
+	# copy the RGB image
+	src_img = root + '/' + f[:-8] + '.jpg'
+	copy2(src_img, dest)
+
+	# create binary mask for wall
+	src_seg = root + '/' + f[:-8] + '_seg.png'
+	img = cv2.imread(src_seg)
+	h, w, c = img.shape
+	for i in range(h):
+		for j in range(w):
+			if img[i, j, 1] == 162:
+				img[i, j] = [255, 255, 255]
+			else:
+				img[i, j] = [0, 0, 0]
+
+	# write the mask
+	cv2.imwrite(dest + '/' + f[:-8] + '_seg.png', img)
+	print(dest + '/' + f[:-8] + '_seg.png')
 
 #go through all the folders
 for (root,dirs,files) in os.walk(ade20k_path, topdown=True): 
@@ -20,12 +45,16 @@ for (root,dirs,files) in os.walk(ade20k_path, topdown=True):
 		with open(root + '/' + f) as file:  
 			data = file.read() 
 			if all(labels in data for labels in  labels_to_filter):
-				# print (root)
-				# print (f)
 				file_count += 1
-				src_img = root + '/' + f[:-8] + '.jpg'
-				copy2(src_img, dest)
-				src_seg = root + '/' + f[:-8] + '_seg.png'
-				copy2(src_seg, dest)
+				if __name__ == '__main__':
+					p = multiprocessing.Process( target = write_binary_mask, args =(root, f, dest))
+					p.start()
+					processes.append(p)
+				
+				# if file_count == 10:
+				# 	break
+	if __name__ == '__main__':
+		for process in processes:
+			process.join()
 	print ('--------------------------------')
 print("found " + str(file_count) + " files")
