81f8b2d1580eedcb364040d3461ad1de3d271b58
bogdan-stanciulescu <53901758+bogdan-s@users.noreply.github.com>
2019-10-24 18:55:48 +0300

    Update filter_ADE20K_dataset.py

--- filter_ADE20K_dataset.py
+++ filter_ADE20K_dataset.py
@@ -1,7 +1,7 @@
 import os 
 from shutil import copy2
 
-ade20k_path = 'D:/ADE20K_2016_07_26/images/training/a/abbey' # where the ADE20K dataset is
+ade20k_path = 'D:/Python/Datasets/ADE20K_2016_07_26/images/training/a/art_gallery' # where the ADE20K dataset is
 # ade20k_path = 'D:/ADE20K_2016_07_26/images/training' # where the ADE20K dataset is
 
 dest = 'D:/Python/DataSets/ADE20K_Filtered' # where the filtered files are copied
@@ -17,7 +17,7 @@ for (root,dirs,files) in os.walk(ade20k_path, topdown=True):
 	txt_files = [f for f in files if f.endswith(".txt")]
 	
 	for f in txt_files:
-		with open(src) as file:  
+		with open(root + '/' + f) as file:  
 			data = file.read() 
 			if all(labels in data for labels in  labels_to_filter):
 				# print (root)
