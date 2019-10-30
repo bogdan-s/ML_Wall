0831ef9e8ce8d1f2dafb8e11e66df1abf2198642
bogdan-stanciulescu <53901758+bogdan-s@users.noreply.github.com>
2019-10-30 22:42:28 +0200

    Loading Images
    added image loading from dir with ImageDataGenerator

--- TF2_GPU.py
+++ TF2_GPU.py
@@ -12,43 +12,70 @@ print(tf.__version__)
 
 from keras.preprocessing.image import ImageDataGenerator
 
-# from keras.applications.resnet50 import ResNet50
-# from keras.preprocessing import image
-# from keras.applications.resnet50 import preprocess_input, decode_predictions
+
 
 # From KERAS.IO:
 # Example of transforming images and masks together.
 # we create two instances with the same arguments
-data_gen_args = dict(featurewise_center=True,
-                     featurewise_std_normalization=True,
-                     rotation_range=10,
-                     width_shift_range=0.1,
-                     height_shift_range=0.1,
-                     zoom_range=0.2)
+data_gen_args = dict(
+                    rescale=1./255,
+                    shear_range=0.2,
+                    zoom_range=0.2,
+                    horizontal_flip=True)
+
 image_datagen = ImageDataGenerator(**data_gen_args)
 mask_datagen = ImageDataGenerator(**data_gen_args)
 
 # Provide the same seed and keyword arguments to the fit and flow methods
 seed = 1
-image_datagen.fit(images, augment=False, seed=seed)
-mask_datagen.fit(masks, augment=False, seed=seed)
 
-image_generator = image_datagen.flow_from_directory(
-    'D:/Python/DataSets/ADE20K_Filtered/Train/Images',
+# Train & Validation images and masks
+train_image_generator = image_datagen.flow_from_directory(
+    '../DataSets/ADE20K_Filtered/Train/Images',
+    target_size=(150, 150),
+    batch_size=32,
     class_mode=None,
+    shuffle=True,
     seed=seed)
-
-mask_generator = mask_datagen.flow_from_directory(
-    'D:/Python/DataSets/ADE20K_Filtered/Train/Masks',
+val_image_generator = image_datagen.flow_from_directory(
+    '../DataSets/ADE20K_Filtered/Validation/Images',
+    target_size=(150, 150),
+    batch_size=32,
     class_mode=None,
+    shuffle=True,
+    seed=seed)
+train_mask_generator = mask_datagen.flow_from_directory(
+    '../DataSets/ADE20K_Filtered/Train/Masks',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode=None,
+    shuffle=True,
+    seed=seed)
+val_mask_generator = mask_datagen.flow_from_directory(
+    '../DataSets/ADE20K_Filtered/Validation/Masks',
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode=None,
+    shuffle=True,
     seed=seed)
 
 # combine generators into one which yields image and masks
-train_generator = zip(image_generator, mask_generator)
+train_generator = zip(train_image_generator, train_mask_generator)
+val_generator = zip(val_image_generator, val_mask_generator)
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=2000,
-    epochs=50)
+
+
+
+
+
+
+
+
+
+
+# model.fit_generator(
+#     train_generator,
+#     steps_per_epoch=2000,
+#     epochs=50)
 
 
